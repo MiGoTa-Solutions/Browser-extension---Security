839e9e5decc4c43a888f8027b311ecd74e5abff5
gokul1111-cmd <gokulanbalagan1112@gmail.com>
2025-11-30 22:58:44 +0530

    fix: error files in web access lock module

--- public/background.js
+++
@@ -1,4 +0,0 @@
-// Entry point for background script
-// This file will be built from src/extension/background.ts
-
-importScripts('./src/extension/background.js');
--- public/content.js
+++
@@ -1,5 +0,0 @@
-// Entry point for content script
-// This file will be built from src/extension/content.ts
-
-// Placeholder content script
-console.log('SecureShield: Content script loaded');
